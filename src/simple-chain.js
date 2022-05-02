8b50c00b957d3213293a9f9a6a16e33455c7e090
maksimizer <maximorlov@mail.ru>
2022-05-02 15:19:57 +0300

    feat: solve simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chainArr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chainArr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chainArr.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof (position) !== 'number' || !Number.isInteger(position) || position < 1 || position >= this.chainArr.length) {
+      this.chainArr.length = 0;
+      throw new Error("You can't remove incorrect link!");
+    };
+    this.chainArr.splice(position -1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chainArr.reverse();
+    return this;    
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let chain = this.chainArr.join("~~");
+    this.chainArr.length = 0;
+    return chain;
   }
 };
 
