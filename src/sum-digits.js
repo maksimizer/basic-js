67704dd5c3555561f2098239288d30ad51c97cca
maksimizer <maximorlov@mail.ru>
2022-05-02 14:26:08 +0300

    feat: solve common-character-count task

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let nStr = n.toString();
+  let result = 0;
+  for (let i = 0; i < nStr.length; i++) {
+    result = result + Number(nStr[i]);
+  };
+  if (result > 9) {
+    return getSumOfDigits(result);
+  } else {
+    return result
+  };
 }
 
 module.exports = {
